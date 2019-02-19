2795cad9f1e2b5c58fa89e4f2a71d53e921beac7
jan nietfeld <jan@meltwater.com>
2019-02-19 15:41:00 +0100

    Fix license.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExLSH.MixProject do
     [
       app: :ex_lsh,
       description: description(),
-      version: "0.2.0",
+      version: "0.4.0",
       elixir: "~> 1.4",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -22,14 +22,10 @@ defmodule ExLSH.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:matrex, git: "https://github.com/versilov/matrex.git", ref: "7abf3fdb79dd1fad8aa077d00e3ff96d4c21ea4e"},
@@ -43,9 +39,9 @@ defmodule ExLSH.MixProject do
 
   defp package() do
     [
-      licenses: ["Apache 2.0"],
+      licenses: ["MIT"],
       links: %{ "GitHub" => "https://github.com/meltwater/ex_lsh" },
-      organization: "meltwater",
+      organization: "Meltwater",
     ]
   end
 end
